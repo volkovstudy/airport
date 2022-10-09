f915ef72e50b1fda304fb085a7e0c31c5f748309
Volkov Daniil <volkov.study@mail.ru>
2022-10-09 19:03:22 +0300

    Add project logic in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include "Time/Time.h"
+#include "TimeConverter/TimeConverter.h"
 
 using namespace std;
 
+Time* getArrivalTime(Time* departureTime, Time* timeInFly);
+
 int main() {
+    string departureTimeString, timeInFlyString;
+    cin >> departureTimeString >> timeInFlyString;
+
+    Time* departureTime = TimeConverter::convertFromString(departureTimeString);
+    Time* timeInFly = TimeConverter::convertFromString(timeInFlyString);
+
+    Time* arrivalTime = getArrivalTime(departureTime, timeInFly);
+
+    cout << departureTimeString << " ";
+    cout << timeInFlyString << " ";
+    cout << arrivalTime->toString() << endl;
+
     return 0;
+}
+
+Time* getArrivalTime(Time* departureTime, Time* timeInFly) {
+    departureTime->addHours(timeInFly->getHours());
+    departureTime->addMinutes(timeInFly->getMinutes());
+
+    int arrivalHours = departureTime->getHours();
+    int arrivalMinutes = departureTime->getMinutes();
+
+    Time* arrivalTime = new Time(arrivalHours, arrivalMinutes);
+
+    return arrivalTime;
 }
