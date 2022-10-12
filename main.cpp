f2bf3507a4a6e8baf9caf318ba7e7796c40af56f
Volkov Daniil <volkov27daniil@yandex.ru>
2022-10-12 21:44:18 +0300

    Add using of TimeAdder class to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include "Time/Time.h"
 #include "TimeConverter/TimeConverter.h"
+#include "TimeAdder/TimeAdder.h"
 
 using namespace std;
-
-Time* getArrivalTime(Time* departureTime, Time* timeInFly);
 
 int main() {
     string departureTimeString, timeInFlyString;
@@ -13,23 +12,11 @@ int main() {
     Time* departureTime = TimeConverter::convertFromString(departureTimeString);
     Time* timeInFly = TimeConverter::convertFromString(timeInFlyString);
 
-    Time* arrivalTime = getArrivalTime(departureTime, timeInFly);
+    Time* arrivalTime = TimeAdder::sumTimes(departureTime, timeInFly);
 
     cout << departureTimeString << " ";
     cout << timeInFlyString << " ";
     cout << arrivalTime->toString() << endl;
 
     return 0;
-}
-
-Time* getArrivalTime(Time* departureTime, Time* timeInFly) {
-    int departureHours = departureTime->getHours();
-    int departureMinutes = departureTime->getMinutes();
-
-    Time* arrivalTime = new Time(departureHours, departureMinutes);
-
-    arrivalTime->addHours(timeInFly->getHours());
-    arrivalTime->addMinutes(timeInFly->getMinutes());
-
-    return arrivalTime;
 }
